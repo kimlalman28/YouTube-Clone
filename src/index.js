8f8e88ab441d3f77698b46fab458e65d8bec3e53
Kimberly Lalmansingh <kimlalman28@gmail.com>
2018-01-25 23:16:14 -0500

    added video list items

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 const mykey = config.MY_KEY;
 
-const App = () => {
-	return <div> Hi! </div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({
+			key: mykey,
+			term: 'dashiexp'}, 
+			(videos) => {
+				this.setState({ videos }); //this.setState({videos:videos}) key and property have same name
+			});
+		}
+
+	render() {
+		return ( 
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
