c2337502ad4e1d829d314b91e9ab860dd704cf56
Kimberly Lalmansingh <kimlalman28@gmail.com>
2018-01-28 23:15:45 -0500

    implemented video searching functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -17,9 +18,13 @@ class App extends Component {
 
 		};
 
+		this.videoSearch('dashiexp');
+	}
+
+	videoSearch(term) {
 		YTSearch({
 			key: mykey,
-			term: 'dashiexp'}, 
+			term: term}, 
 			(videos) => {
 				this.setState({
 					videos: videos,
@@ -29,9 +34,11 @@ class App extends Component {
 		}
 
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return ( 
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList 
 					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
